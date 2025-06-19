157b098014a038bfbe0d4ad21f3634acc6bfc5a7
Aaliya116 <msaaliyaaaliya705@ms.du.ac.in>
2025-06-19 12:27:14 +0530

    app.py

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ st.markdown("Enter the details below to predict the price of a house:")
 area = st.number_input("Area (in square feet)", min_value=100, max_value=10000, value=1500)
 # Prediction
 if st.button("Predict Price"):
-    input_data = [[area, bedrooms, age]]
+    input_data = [[area]]
     prediction = model.predict(input_data)
     st.success(f"🏡 Estimated Home Price: ₹{prediction[0]:,.2f}")
